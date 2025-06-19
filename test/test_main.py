7e8804cfcc41fe6cd851f2496ffe946c13470b94
bribrix <briac.marchandise@gmail.com>
2025-06-19 19:18:59 +0200

    Modif unit test 2

--- test/test_main.py
+++ test/test_main.py
@@ -6,7 +6,8 @@ client = TestClient(app)
 def test_submit_form():
     response = client.post(
         "/form",
-        data={"name":"Alice"},
-        headers={"Content-Type": "application/x-www-form-urlencoded"})
+        data={"name": "Alice"},
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, Alice"}
+    assert response.json() == {"message": "Hello, Alice!"}
