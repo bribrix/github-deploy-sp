cab14ad9982a21055736b4127b75da334ef89f55
Bribrix <briac.marchandise@gmail.com>
2025-07-01 14:55:36 +0200

    Add ML pipeline and prediction model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import joblib
+import os
 import json
 
 app = FastAPI()
@@ -13,22 +16,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class FormData(BaseModel):
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "model_heart.pkl")
+model = joblib.load(MODEL_PATH)
+
+class PatientData(BaseModel):
     age: int
-    gender: int
+    sex: int
     cp: int
-    bp: int
-    cholesterol: int
+    trestbps: int
+    chol: int
     fbs: int
-    ecg: int
-    mhr: int
+    restecg: int
+    thalach: int
     exang: int
     oldpeak: float
     slope: int
     ca: int
     thal: int
 
-@app.post("/form")
-def receive_form(data: FormData):
+@app.post("/predict")
+def predict(data: PatientData):
+    input_data = input_df = pd.DataFrame([{             #passer un dataframe pandas en entrée du modèle 
+        "age": data.age,
+        "sex": data.sex,
+        "cp": data.cp,
+        "trestbps": data.trestbps,
+        "chol": data.chol,
+        "fbs": data.fbs,
+        "restecg": data.restecg,
+        "thalach": data.thalach,
+        "exang": data.exang,
+        "oldpeak": data.oldpeak,
+        "slope": data.slope,
+        "ca": data.ca,
+        "thal": data.thal
+    }])
+    prediction = model.predict(input_data)
     print(json.dumps(data.dict(), indent=2))
-    return {"message": f"Data received"}
+    return {"message": f"Prediction is :  {int(prediction[0])} with an error of 10% of cases"}
